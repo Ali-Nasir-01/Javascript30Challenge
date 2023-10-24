b47dd8c828e0f159c60e5b091986f4808240bd40
nasir <ali.nasir.0104@gmail.com>
2023-10-24 23:24:45 +0330

    feat: fetch cities

--- 06 - Type Ahead/main.js
+++ 06 - Type Ahead/main.js
@@ -1,2 +1,10 @@
-const endpoint =
+const action =
   "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
+
+const cities = [];
+
+fetch(action)
+  .then((response) => response.json())
+  .then((data) => {
+    cities.push(...data);
+  });
