21e66bd7e68fd9f444e3c839e980284bc47e58bb
nasir <ali.nasir.0104@gmail.com>
2023-11-15 23:41:45 +0330

    feat: complete 09 day challenge

--- 09 - Dev Tools Domination/main.js
+++ 09 - Dev Tools Domination/main.js
@@ -10,25 +10,49 @@ function makeGreen() {
 }
 
 // Regular
-
+console.log("Hello");
 // Interpolated
-
+console.log("Hello I'm %s and my friend is %s", "Ali", "Hamed");
 // Styled
-
+console.log("%c This is styled log", "font-size:50px; color: green");
 // warning!
-
+console.warn("Watch your back!");
 // Error :|
-
+console.error("Buuuuuuuuuuuuuuuuuug");
 // Info
-
+console.info("Animals can experience time differently from humans.");
 // Testing
-
+console.assert(2 > 3, "Hello");
 // clearing
-
+console.clear();
 // Viewing DOM Elements
+const p = document.querySelector("p");
 
+console.log(p);
+console.dir(p);
 // Grouping together
-
+for (const dog of dogs) {
+  console.groupCollapsed(dog.name);
+  console.log(`name: ${dog.name}`);
+  console.log(`age: ${dog.age}`);
+  console.groupEnd(dog.name);
+}
 // counting
-
+console.count("you");
+console.count("me");
+console.count("me");
+console.count("you");
+console.count("me");
+console.count("you");
+console.count("me");
+console.count("me");
+console.count("you");
+console.count("you");
 // timing
+console.time();
+fetch("https://api.github.com/users/wesbos")
+  .then((data) => data.json())
+  .then((data) => {
+    console.timeEnd();
+    console.log(data);
+  });
